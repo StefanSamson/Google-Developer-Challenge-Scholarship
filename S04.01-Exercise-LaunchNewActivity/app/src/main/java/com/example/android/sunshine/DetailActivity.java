d5b418b4c19c9a4f10cabbbb0f4e1fa082c2f6a5
Stefan Samson <stefansamson22@gmail.com>
2017-11-18 20:32:30 +0200

    S04.01-Exercise-LaunchNewActivity - Add up button functionality

--- S04.01-Exercise-LaunchNewActivity/app/src/main/java/com/example/android/sunshine/DetailActivity.java
+++ S04.01-Exercise-LaunchNewActivity/app/src/main/java/com/example/android/sunshine/DetailActivity.java
@@ -1,7 +1,9 @@
 package com.example.android.sunshine;
 
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 public class DetailActivity extends AppCompatActivity {
 
@@ -9,5 +11,16 @@ public class DetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+
+//        // my_child_toolbar is defined in the layout file
+//        Toolbar myChildToolbar =
+//                (Toolbar) findViewById(R.id.my_child_toolbar);
+//        setSupportActionBar(myChildToolbar);
+
+        // Get a support ActionBar corresponding to this toolbar
+        ActionBar ab = getSupportActionBar();
+
+        // Enable the Up button
+        ab.setDisplayHomeAsUpEnabled(true);
     }
 }
